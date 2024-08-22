beb51681eb718f7669c636419de858584bb90e14
Petros Kitsaras <petros.kitsaras@gmail.com>
2024-08-22 17:05:04 +0200

    Intenational laungage service, profiles

--- src/main/java/guru/springframework/sfg_di/SfgDiApplication.java
+++ src/main/java/guru/springframework/sfg_di/SfgDiApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 
 import guru.springframework.sfg_di.controllers.ConstructorInjectedController;
+import guru.springframework.sfg_di.controllers.I18nController;
 import guru.springframework.sfg_di.controllers.MyController;
 import guru.springframework.sfg_di.controllers.PropertyInjectedController;
 import guru.springframework.sfg_di.controllers.SetterInjectedController;
@@ -15,10 +16,13 @@ public class SfgDiApplication {
 	public static void main(String[] args) {
 		ApplicationContext ctx = SpringApplication.run(SfgDiApplication.class,args);
 
+		I18nController i18nController = (I18nController)ctx.getBean("i18nController");
+		System.out.println(i18nController.sayHello());
+
 		MyController myController = (MyController)ctx.getBean("myController");
 
-		String greeting = myController.sayHello();
-		System.out.println(greeting);
+		System.out.println("----- Primary Bean");
+		System.out.println(myController.sayHello());
 
 		System.out.println("----- Property");
 		PropertyInjectedController propertyInjectedController = (PropertyInjectedController)ctx.getBean("propertyInjectedController");
--- src/main/java/guru/springframework/sfg_di/controllers/I18nController.java
+++ src/main/java/guru/springframework/sfg_di/controllers/I18nController.java
@@ -1,18 +1,22 @@
 package guru.springframework.sfg_di.controllers;
 
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 
 import guru.springframework.sfg_di.services.GreetingService;
 
 @Controller
-public class MyController 
+public class I18nController  
 {
     private final GreetingService greetingService;
-    public MyController(GreetingService greetingService){
+
+    public I18nController(@Qualifier("i18nService")GreetingService greetingService)
+    {
         this.greetingService = greetingService;
     }
-   public String sayHello()
-   {
-    return greetingService.sayGreeting();
-   }
+
+    public String sayHello()
+    {
+        return greetingService.sayGreeting();
+    }
 }
