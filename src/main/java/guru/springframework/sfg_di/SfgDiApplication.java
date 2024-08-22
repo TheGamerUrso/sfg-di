c8af772197d73f1eabf1945d7d15a5edcce95460
Petros Kitsaras <petros.kitsaras@gmail.com>
2024-08-22 16:43:29 +0200

    Use of Qualifier , added Controllers , added service

---
+++ src/main/java/guru/springframework/sfg_di/SfgDiApplication.java
@@ -0,0 +1,36 @@
+package guru.springframework.sfg_di;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+import guru.springframework.sfg_di.controllers.ConstructorInjectedController;
+import guru.springframework.sfg_di.controllers.MyController;
+import guru.springframework.sfg_di.controllers.PropertyInjectedController;
+import guru.springframework.sfg_di.controllers.SetterInjectedController;
+
+@SpringBootApplication
+public class SfgDiApplication {
+
+	public static void main(String[] args) {
+		ApplicationContext ctx = SpringApplication.run(SfgDiApplication.class,args);
+
+		MyController myController = (MyController)ctx.getBean("myController");
+
+		String greeting = myController.sayHello();
+		System.out.println(greeting);
+
+		System.out.println("----- Property");
+		PropertyInjectedController propertyInjectedController = (PropertyInjectedController)ctx.getBean("propertyInjectedController");
+		System.out.println(propertyInjectedController.getGreeting());
+		
+		System.out.println("----- Setter");
+		SetterInjectedController setterInjectedController = (SetterInjectedController)ctx.getBean("setterInjectedController");
+		System.out.println(setterInjectedController.getGreeting());
+
+		System.out.println("----- Constructor");
+		ConstructorInjectedController constructorInjectedController = (ConstructorInjectedController)ctx.getBean("constructorInjectedController");
+		System.out.println(constructorInjectedController.getGreeting());
+	}
+
+}
---
+++ src/main/java/guru/springframework/sfg_di/services/GreetingService.java
@@ -0,0 +1,8 @@
+package guru.springframework.sfg_di.services;
+
+import org.springframework.stereotype.Service;
+
+
+public interface GreetingService {
+    String sayGreeting();
+}
